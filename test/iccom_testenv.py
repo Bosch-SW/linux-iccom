23fdbb0e92dadb5c746888c4bc5236e55ef657d7
Artem Gulyaev <Artem.Gulyaev@de.bosch.com>
2024-09-17 12:26:36 +0000

    [tests] Extended the ICCom testing environment
    * added standard ICCom data wire IO simulation
      methods,
    * added the ICCom statistics printout.

--- test/iccom_testenv.py
+++ test/iccom_testenv.py
@@ -7,7 +7,7 @@ import string
 import random
 import glob
 
-import iccom
+from iccom import *
 import iccom_skif
 
 # Just a convenience wrapper to handle the default test environment
@@ -24,6 +24,10 @@ class IccomTestEnv:
                self.iccom_test_transport_dev_type = "fd_test_transport"
 
                self.skip_iccom_skif_device = skip_iccom_skif_device
+        
+               # initial indexes of the iccom for internal confirmations
+               self.inbound_package_num = 1
+               self.outbound_package_num = 1
 
         def get_one_dev_name(dev_type):
 
@@ -84,8 +88,8 @@ class IccomTestEnv:
 
                 if not self.skip_iccom_skif_device:
                         iccom_skif.create_iccom_sockets_device(None)
-                iccom.create_iccom_device(None)
-                iccom.create_fd_test_transport_device(None)
+                create_iccom_device(None)
+                create_fd_test_transport_device(None)
 
                 if not self.skip_iccom_skif_device:
                         self.iccom_skif_dev_name = self.get_one_iccom_skif_name()
@@ -96,7 +100,7 @@ class IccomTestEnv:
 
                 print("Linking the stack.")
 
-                iccom.link_fd_test_transport_device_to_iccom_device(
+                link_fd_test_transport_device_to_iccom_device(
                         self.iccom_transport_dev_name, self.iccom_dev_name, None)
                 if not self.skip_iccom_skif_device:
                         iccom_skif.set_socket_protocol_family(
@@ -109,14 +113,16 @@ class IccomTestEnv:
         # Tests helper.
         # Unbinds and removes default devices set.
         def iccom_destroy_default_stack(self):
+                self.print_iccom_statistics()
+
                 print("Destroying default test env.")
 
                 if not self.skip_iccom_skif_device:
                         iccom_skif.delete_iccom_sockets_device(
                                 self.iccom_skif_dev_name, None)
-                iccom.delete_iccom_device(
+                delete_iccom_device(
                       self.iccom_dev_name, None)
-                iccom.delete_fd_test_transport_device(
+                delete_fd_test_transport_device(
                       self.iccom_transport_dev_name, None)
 
                 print("Default test env destroyed.")
@@ -127,4 +133,63 @@ class IccomTestEnv:
 
         def __exit__(self, exc_type, exc_value, traceback):
                 self.iccom_destroy_default_stack()
- 
+
+        # Checks that the given data (fitting into one package) is received via
+        # ICCom.
+        # @te IccomTestEnv
+        def iccom_simulate_incoming(self, channel, data):
+                # received it properly
+                check_wire_xfer(self.test_transport_name()
+                        , iccom_package(self.inbound_package_num
+                                        , iccom_packet(channel
+                                                , data
+                                                , True))
+                        , iccom_package(self.outbound_package_num, bytearray())
+                        , None, "single data frame")
+                check_wire_xfer(self.test_transport_name()
+                        , iccom_ack_package()
+                        , iccom_ack_package()
+                        , None, "ack frame")
+
+                self.inbound_package_num += 1
+                self.outbound_package_num += 1
+        
+        # Checks that the given data (fitting into one package) is sent out via ICCom.
+        # @te IccomTestEnv
+        def iccom_check_simple_write(self, channel, data):
+                # Do (Default xfer) Data Exchange + NACK
+                check_wire_xfer(self.test_transport_name()
+                                , iccom_package(self.inbound_package_num
+                                                , bytearray())
+                                , iccom_package(self.outbound_package_num
+                                                , bytearray())
+                                , None, None, "first data frame")
+                check_wire_xfer_ack(self.test_transport_name()
+                                        , None, None, "first ack frame")
+
+                self.inbound_package_num += 1
+                self.outbound_package_num += 1
+        
+                # ICCom sends correct data, but we complain that we have not
+                # received it properly
+                check_wire_xfer(self.test_transport_name()
+                        , iccom_package(self.inbound_package_num
+                                        , bytearray())
+                        , iccom_package(self.outbound_package_num
+                                        , iccom_packet(channel
+                                                , data
+                                                , True))
+                        , None, "second data frame")
+                check_wire_xfer(self.test_transport_name()
+                        , iccom_ack_package()
+                        , iccom_ack_package()
+                        , None, "ack frame")
+
+        def print_iccom_statistics(self):
+                fn = "/sys/devices/platform/" + self.get_one_iccom_name() + "/statistics"
+
+                print("ICCom %s statistics:" % (fn,))
+                with open(fn, "r") as statistics:
+                        for line in statistics.readlines():
+                                print("    | " + line)
+
