c56d1389edb07e3e4c45b044d3dd7554572af19f
Artem Gulyaev <Artem.Gulyaev@de.bosch.com>
2025-10-28 16:51:48 +0000

    [utils] ICCom package parsing util updated
    * now it uses the parsing provided by iccom.py which
      has almost full functionality matching that of the driver,
      so the parsing is now much more informative and covers
      the bad packages as well.

--- tools/iccom-package-parser.py
+++ tools/iccom-package-parser.py
@@ -1,49 +1,48 @@
 import sys
 import json
+import os
 
-# Parses ICCom package into human readable data.
-# For now just prints it to stdout.
-# NOTE: For now just handles the proper-formed packages.
-def iccom_parse(data_package_bytes):
-    package_payload_size = int.from_bytes(data_package_bytes[0:2], 'big')
-    package_sequence_id = int.from_bytes(data_package_bytes[2:3], 'little')
-    ppb = data_package_bytes[3:3 + package_payload_size]
+tools_dir_path = os.path.dirname(os.path.realpath(__file__))
+iccom_repo_root = os.path.realpath(os.path.join(tools_dir_path, "../"))
+test_dir_path = os.path.join(iccom_repo_root, "test/")
+sys.path.append(test_dir_path)
 
-    parsing_result = {
-        "package_payload_size": package_payload_size
-        , "package_sequence_id": package_sequence_id
-        , "packets": []
-    }
-
-    pkt_off = 0
-    while (pkt_off < package_payload_size):
-        p_size = int.from_bytes(ppb[pkt_off : pkt_off + 2], 'big')
-        hi_ch = int.from_bytes(ppb[pkt_off + 2 : pkt_off + 3], 'big')
-        lo_ch = int.from_bytes(ppb[pkt_off + 3 : pkt_off + 4], 'big') & 0x7F
-        ch = (hi_ch << 7) | lo_ch
-        complete = ((int.from_bytes(ppb[pkt_off + 3 : pkt_off + 4], 'big') & 0x80) == 0x80)
-
-        pkt = {
-            "ch": ch
-            , "complete": complete
-            , "data_size": p_size
-            , "data": ppb[pkt_off + 4: pkt_off + 4 + p_size].hex()
-        }
-        parsing_result["packets"].append(pkt)
-
-        pkt_off += 4 + p_size
-
-    print(json.dumps(parsing_result))
-
-    return parsing_result
-
-if (len(sys.argv) < 2):
-    print("Hello, I parse ICCom data packages, pls give me a hex string as first arg.\n")
-    print(" example:  python3 parser.py 001801000200957473000200827473000200837473000200847473ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbfa6d7a7\n\n")
+try:
+    import iccom
+except e:
+    print("It looks like you don't have the iccom.py file available"
+          "the python PATH nor in %s folder. I'm expected either\n"
+          "* to get launched directly from the iccom repository\n"
+          "* or to have the iccom.py file next to me or in\n"
+          "  default python path (iccom.py is located in the\n"
+          "  <ICCOM_REPO_ROOT>/test/iccom.py)\n" % (test_dir_path,))
     exit()
 
-package_hex_string = sys.argv[1]
-package_bytes = bytearray.fromhex(package_hex_string)
+# proper serialization for the bytearrays
+class BytearrayEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytearray):
+            return str(obj.hex())
+        else:
+            super().default(obj)
 
-print("Parsing: %s\n" % package_hex_string)
-iccom_parse(package_bytes)
+def main_parsing():
+    if (len(sys.argv) < 2):
+        print("Hello, I parse ICCom data packages, pls give me"
+            " a hex string as first arg.\n")
+        print(" example:  python3 iccom-package-parser.py "
+            "00180100020095747300020082747300"
+            "0200837473000200847473ffffffffff"
+            "ffffffffffffffffffffffffffffffff"
+            "ffffffffffffffffffffffffbfa6d7a7"
+            "\n\n")
+        exit()
+
+    package_hex_string = sys.argv[1]
+    package_bytes = bytearray.fromhex(package_hex_string)
+
+    print("Parsing: %s\n" % package_hex_string)
+    res = iccom.iccom_package_parse(package_bytes, pedantic=True)
+    print(json.dumps(res, indent=4, cls=BytearrayEncoder))
+
+main_parsing()
